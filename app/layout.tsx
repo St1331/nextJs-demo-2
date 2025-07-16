89d044c9c9a50b14afdb99bd43e960494ec85c5b
Tan Shi <heyost980131@gmail.com>
2025-07-16 18:44:06 -0400

    next demo learning . now learn database setting up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import {inter} from "@/app/ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    {/*//调用inter（）时，nextJs会返回一个对象，包含了一些可以在组件中使用的属性*/}
     </html>
   );
 }
